dc8682064632dfe3c3e245a661740ab18fadc489
Anthony Catullo <acatullo4@gmail.com>
2021-06-23 18:10:20 -0500

    added controllers

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,31 @@
 class TicketsController < ApplicationController
+
+    def index 
+        tickets = Ticket.all
+        render json: tickets
+    end 
+
+    def create 
+        ticket = Ticket.create(ticket_params)
+        render json: ticket
+    end 
+
+    def update 
+        ticket = Ticket.find(params[:id])
+        ticket.update(ticket_params) 
+        render json: ticket
+    end
+
+    def destroy 
+        ticket = Ticket.find(params[:id])
+        ticket.destroy
+        render json: {}
+    end 
+
+    private 
+
+    def ticket_params
+        params.require(:ticket).permit(:status, :title, :description, :ticket_type, :effort)
+    end 
+
 end
