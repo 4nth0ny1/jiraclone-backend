4b72139ea88696d1debdd9988db04d957c9be366
Anthony Catullo <acatullo4@gmail.com>
2021-06-23 18:15:20 -0500

    backend completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :tickets
+  resources :comments, except: [:index]
+  resources :tickets do 
+    resources :comments, only: [:index]
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
