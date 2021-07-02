72b9a1276b5977982a1c0eaf0f719eb23cf2ad9a
Anthony Catullo <acatullo4@gmail.com>
2021-07-02 12:06:37 -0500

    updated backend for errors

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
+  belongs_to :ticket
   attributes :id, :content, :ticket_id
 end
--- app/serializers/ticket_serializer.rb
+++ app/serializers/ticket_serializer.rb
@@ -1,3 +1,4 @@
 class TicketSerializer < ActiveModel::Serializer
+  has_many :comments
   attributes :id, :status, :title, :description, :ticket_type, :effort
 end
