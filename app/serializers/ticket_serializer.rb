1a3b43ae9ca36741f17d58d27d7e8af23a1eebbe
Anthony Catullo <acatullo4@gmail.com>
2021-08-08 10:53:10 -0500

    add last updated

--- app/serializers/ticket_serializer.rb
+++ app/serializers/ticket_serializer.rb
@@ -1,8 +1,13 @@
 class TicketSerializer < ActiveModel::Serializer
   has_many :comments
-  attributes :id, :status, :title, :description, :ticket_type, :effort, :priority, :formatted_created_at
+  attributes :id, :status, :title, :description, :ticket_type, :effort, :priority, :formatted_created_at, :last_updated_at
 
   def formatted_created_at 
       Time.use_zone('America/Chicago') {object.created_at.strftime("%m/%d/%Y, %I:%M%p")}
   end 
+
+  def last_updated_at 
+    Time.use_zone('America/Chicago') {object.updated_at.strftime("%m/%d/%Y, %I:%M%p")}
+  end 
+
 end
