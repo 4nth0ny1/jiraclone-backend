a879759eeecfe597d073187a19c5f2c48e3e7a5c
Anthony Catullo <acatullo4@gmail.com>
2021-08-08 10:37:25 -0500

    add time zone

--- app/serializers/ticket_serializer.rb
+++ app/serializers/ticket_serializer.rb
@@ -1,4 +1,8 @@
 class TicketSerializer < ActiveModel::Serializer
   has_many :comments
-  attributes :id, :status, :title, :description, :ticket_type, :effort, :priority
+  attributes :id, :status, :title, :description, :ticket_type, :effort, :priority, :formatted_created_at
+
+  def formatted_created_at 
+      Time.use_zone('America/Chicago') {object.created_at.strftime("%m/%d/%Y, %I:%M%p")}
+  end 
 end
